bfa3f54aae18da87498635d5e7390cf236de8b45
gaoming <924806524@qq.com>
2023-05-14 22:07:39 +0800

    runtime

---
+++ build.js
@@ -0,0 +1,21 @@
+// const webpack = require('webpack')
+const webpack = require('webpack')
+const path = require('path')
+
+
+function f1 () {
+  return webpack({
+    entry: './index.js',
+    mode: 'none',
+    output: {
+      iife: false,
+      pathinfo: 'verbose'
+    }
+
+  })
+}
+
+
+f1().run((err, stat) => {
+  // console.log(JSON.stringify(stat.toJson()))
+})
